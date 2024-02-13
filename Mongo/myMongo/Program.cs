6c5e39312f6b349ca5612d0bd980f4206cc0167c
Rohanmrao <rohanmaheshrao10@gmail.com>
2024-02-13 15:39:53 +0530

    connection string test print from json object

---
+++ Mongo/myMongo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using System;
+using System.IO;
+
+public class mongoConfig
+{
+    public string mongoconnectionString { get; set; }
+}
+namespace ProgramMongo
+{
+    public class Program()
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string jsonString = File.ReadAllText("mongoConfig.json");
+            mongoConfig myconfigObj = JsonSerializer.Deserialize<mongoConfig>(jsonString);
+
+            Console.WriteLine($" The connection string is {myconfigObj.mongoconnectionString}");
+        }
+    }
+
+}
+
